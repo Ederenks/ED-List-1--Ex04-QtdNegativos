0560cca4b6213ab82debb3eb2e597e5f40d08990
Edu <honorio.edu2020@gmail.com>
2026-02-23 00:45:01 -0300

    Comit

--- src/Ex04_QtdNegativos/Controller.java
+++ src/Ex04_QtdNegativos/Controller.java
@@ -2,13 +2,13 @@ package Ex04_QtdNegativos;
 
 public class Controller {
 	public int Verificar(int []x, int i, int c  ) {
-		if (i<0) {
+		if (i<0) {//Quando o vetor chegar no ultimo termo, retorna o valor do contador
 			return c;
 		}else {
-			 if (x[i] < 0) {
+			 if (x[i] < 0) {//Se o numero da posição i for negativo, acrescenta 1 no contador
 				 return Verificar(x, i - 1, c + 1);
 			 }else {
-				 return Verificar(x,i-1,c);
+				 return Verificar(x,i-1,c);//caso o numero da posição i não for negativo, prossegue para proxima posiçao
 			 }
 		}
 	
